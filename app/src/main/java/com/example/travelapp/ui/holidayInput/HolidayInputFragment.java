04ba6608253fcab437bdca0a20ff848c25b65da9
MitchellFeaver <feavermr@aston.ac.uk>
2020-02-14 00:27:10 +0000

    test commit 2

--- app/src/main/java/com/example/travelapp/ui/holidayInput/HolidayInputFragment.java
+++ app/src/main/java/com/example/travelapp/ui/holidayInput/HolidayInputFragment.java
@@ -15,7 +15,6 @@ import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
