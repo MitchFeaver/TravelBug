e74d479a62ebbeb896b9190c9f774d5cea0d6f9f
MitchellFeaver <feavermr@aston.ac.uk>
2020-02-17 22:54:06 +0000

    datePicker Not complete -  need 2 datepickers within HolidayInputFragment  need to save to SQLite

---
+++ app/src/main/java/com/example/travelapp/ui/holidayInput/DatePickerFragment.java
@@ -0,0 +1,29 @@
+package com.example.travelapp.ui.holidayInput;
+
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.os.Bundle;
+
+
+import androidx.fragment.app.DialogFragment;
+
+import java.util.Calendar;
+
+public class DatePickerFragment extends DialogFragment {
+
+    private DatePickerDialog.OnDateSetListener dateSetListener; // listener object to get calling fragment listener
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+        // Use the current date as the default date in the picker
+        final Calendar c = Calendar.getInstance();
+        int year = c.get(Calendar.YEAR);
+        int month = c.get(Calendar.MONTH);
+        int day = c.get(Calendar.DAY_OF_MONTH);
+        dateSetListener = (DatePickerDialog.OnDateSetListener) getTargetFragment(); // getting passed fragment
+        return new DatePickerDialog(getActivity(), dateSetListener, year, month, day); // DatePickerDialog gets callBack listener as 2nd parameter
+        // Create a new instance of DatePickerDialog and return it
+    }
+
+}
--- app/src/main/java/com/example/travelapp/ui/holidayInput/HolidayInputFragment.java
+++ app/src/main/java/com/example/travelapp/ui/holidayInput/HolidayInputFragment.java
@@ -1,10 +1,12 @@
 package com.example.travelapp.ui.holidayInput;
 
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProviders;
 import androidx.navigation.NavController;
@@ -15,6 +17,8 @@ import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -28,15 +32,22 @@ import com.google.android.material.snackbar.Snackbar;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class HolidayInputFragment extends Fragment {
+public class HolidayInputFragment extends Fragment implements DatePickerDialog.OnDateSetListener {
     private HolidayViewModel mHolidayViewModel ;
     private EditText mEditHolidayView;
     private NavController navController;
+    private Button startDateButton;
+    private Button endDateButton;
     private TextView startDateText;
-//    private TextView endDateText;
+    private TextView endDateText;
 
     public HolidayInputFragment() {
         // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
     }
 
     @Override
@@ -46,18 +57,25 @@ public class HolidayInputFragment extends Fragment {
         final View v = inflater.inflate(R.layout.fragment_holiday_input, container, false);
         mEditHolidayView = v.findViewById(R.id.holidayName);
         mHolidayViewModel = ViewModelProviders.of(this).get(HolidayViewModel.class );
+
+        startDateButton = v.findViewById(R.id.startDateButton);
+        startDateText = v.findViewById(R.id.startDateText);
+        endDateButton = v.findViewById(R.id.endDateButton);
+        endDateText = v.findViewById(R.id.endDateText);
+
+
         FloatingActionButton fab = getActivity().findViewById(R.id.fab);
         fab.setImageResource (R.drawable.ic_save_black_24dp);
         if (fab != null) {
-            fab.setOnClickListener(new View . OnClickListener () {
+            fab.setOnClickListener(new View.OnClickListener() {
                 @Override
-                public void onClick ( View view ) {
+                public void onClick(View view) {
                     if (TextUtils.isEmpty(mEditHolidayView.getText())) {
                         Snackbar.make(view, " You need to enter a name", Snackbar.LENGTH_LONG)
-                                .setAction(" Action ",null).show();
+                                .setAction(" Action ", null).show();
                     } else {
                         Holiday h = new Holiday(mEditHolidayView.getText().toString());
-                        mHolidayViewModel.insert (h);
+                        mHolidayViewModel.insert(h);
                         NavDirections action =
                                 HolidayInputFragmentDirections.actionHolidayInputToNavHoliday();
                         Navigation.findNavController(v).navigate(action);
@@ -65,6 +83,24 @@ public class HolidayInputFragment extends Fragment {
                 }
             });
         }
+
+        startDateButton.setOnClickListener(new View.OnClickListener() {  // setting listener for user click event
+            @Override
+            public void onClick(View v) {
+                DialogFragment newFragment = new DatePickerFragment();
+                newFragment.show(getFragmentManager(),
+                        getString(R.string.datepicker));
+            }
+        });
+
+        endDateButton.setOnClickListener(new View.OnClickListener() {  // setting listener for user click event
+            @Override
+            public void onClick(View v) {
+                DialogFragment newFragment = new DatePickerFragment();
+                newFragment.show(getFragmentManager(),
+                        getString(R.string.datepicker));
+            }
+        });
         return v;
     }
 
@@ -73,5 +109,13 @@ public class HolidayInputFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         navController = Navigation.findNavController(view);
     }
-}
 
+    @Override
+    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) { // what should be done when a date is selected
+        StringBuilder sb = new StringBuilder().append(dayOfMonth).append("/").append(monthOfYear + 1);
+        String formattedDate = sb.toString();
+        startDateText.setText(formattedDate);
+    }
+
+
+}
